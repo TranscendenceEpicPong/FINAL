85816e1b624462550b1c735fe0e340ff86fa1c21
user@pop-os <mon@email.com>
2024-02-15 16:46:37 +0100

    Fixed with blocks system

--- front/assets/js/input.js
+++
@@ -1,9 +0,0 @@
-function createButton(text, attrs = [])
-{
-	const button = document.createElement('button');
-	for (const attr of attrs)
-		if (attr.name && attr.value)
-			button.setAttribute(attr.name, attr.value);
-	button.innerText = text;
-	return button;
-}
--- front/assets/js/store.js
+++
@@ -1,6 +0,0 @@
-const getUser = () => JSON.parse(localStorage.getItem('user'));
-const setUser = (_user) => {
-	localStorage.setItem('user', JSON.stringify(_user));
-
-	document.getElementById('profile-username').innerText = _user.username;
-}
