7d4f1995a8eaf4466fdd458cd2230681fc03eb69
Your name <mon@email.com>
2024-02-16 16:44:19 +0100

    Add avatar field for users

--- backend/core/models.py
+++ backend/core/models.py
@@ -11,3 +11,4 @@ def random_token():
 class EpicPongUser(AbstractUser):
     token = models.CharField(max_length=128, default=random_token)
     avatar = models.TextField(default="")
+    socket_id = models.CharField(max_length=128, default=random_token)
