7a1602b25d21bac15167b57e02eb648d10508b60
Loïc Humbert <loic@humbert-web.dev>
2024-02-19 17:30:00 +0100

    Clean view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,22 +1,19 @@
 import json
 from typing import Tuple, cast
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
-from django.views.decorators.http import require_POST, require_GET
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 import datetime
 from django.contrib.auth import \
     authenticate as django_authenticate, \
     login as django_login, \
     logout as django_logout, \
-    get_user_model, \
-    get_user
-import base64
+    get_user_model
 from core.models import EpicPongUser
 from .forms import UserRegisterForm, UserLoginForm
 import jwt
 from backend.settings import env
-import io
-from .service import AuthService
+
 
 def perform_auth(request, creds) -> Tuple[JsonResponse, EpicPongUser]:
     user = django_authenticate(request,
@@ -88,35 +85,3 @@ def logout(request):
     response = JsonResponse({"status": "success", "message": "Successfully logged out !"})
     response.delete_cookie('authorization')
     return response
-
-@require_GET
-def request_code(request):
-    user = get_user(request)
-    service = AuthService(user)
-    return JsonResponse({ "status": "success", "message": service.get_code() })
-
-@require_POST
-def a2f_enable(request):
-    try:
-        body = json.loads(request.body)
-    except json.JSONDecodeError:
-        return JsonResponse({"message": "Invalid JSON", "status": 400})
-    if "code" not in body or not body["code"] or type(body['code']) != str or len(body["code"]) != 6:
-        return JsonResponse({"message": "Missing code", "status": 400})
-    user = get_user(request)
-    service = AuthService(user)
-    if service.enable(body['code']):
-        return JsonResponse({ "status": "success", "message": "2FA activé !" })
-    return JsonResponse({ "status": "success", "message": "Error lors de l'activaton de la 2FA" })
-
-@require_POST
-def a2f_disable(request):
-    try:
-        body = json.loads(request.body)
-    except json.JSONDecodeError:
-        return JsonResponse({"message": "Invalid JSON", "status": 400})
-    if "password" not in body or not body["password"] or type(body['password']) != str:
-        return JsonResponse({"message": "Mot de passe requis", "status": 400})
-    user = get_user(request)
-    service = AuthService(user)
-    return JsonResponse(service.disable(body.get('password')))
---
+++ backend/core/config.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class UserConfig(Enum):
+    USERNAME = {"label": "Nom d'utilisateur", "max_length": 20, "min_length": 3, "error_messages": {
+        "required": "Ce champ est obligatoire",
+        "max_length": "Le nom d'utilisateur est trop long (Max 20 caractères)",
+        "min_length": "Le nom d'utilisateur est trop court (Min 3 caractères)"
+    }}
+    AVATAR = {"label": "Avatar", "max_length": 256, "min_length": 3, "error_messages": {
+        "required": "Ce champ est obligatoire",
+        "max_length": "Le lien de l'avatar est trop long (Max 256 caractères)",
+        "min_length": "Le lien de l'avatar est trop court (Min 3 caractères)"
+    }}
+    PASSWORD = {"label": "Mot de passe", "max_length": 256, "min_length": 3, "error_messages": {
+        "required": "Ce champ est obligatoire",
+        "max_length": "Le mot de passe est trop long (Max 256 caractères)",
+        "min_length": "Le mot de passe est trop court (Min 3 caractères)"
+    }}
+    CONFIRM_PASSWORD = {"label": "Confirmation du mot de passe", "max_length": 256, "min_length": 3, "error_messages": {
+        "required": "Ce champ est obligatoire",
+        "max_length": "La confirmationd du mot de passe est trop long (Max 256 caractères)",
+        "min_length": "La confirmationd du mot de passe est trop court (Min 3 caractères)"
+    }}
