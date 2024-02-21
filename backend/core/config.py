4e8258ad229bc696bb19f93a2c1c3505a6ea2eb9
user@pop-os <mon@email.com>
2024-02-21 16:59:43 +0100

    Remove details on error about length of password and avatar

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,21 +4,21 @@ from enum import Enum
 class UserConfig(Enum):
     USERNAME = {"label": "Nom d'utilisateur", "max_length": 20, "min_length": 3, "error_messages": {
         "required": "Ce champ est obligatoire",
-        "max_length": "Le nom d'utilisateur est trop long (Max 20 caractères)",
+        "max_length": "Le nom d'utilisateur est trop long",
         "min_length": "Le nom d'utilisateur est trop court (Min 3 caractères)"
     }}
     AVATAR = {"label": "Avatar", "max_length": 32000, "min_length": 3, "error_messages": {
         "required": "Ce champ est obligatoire",
-        "max_length": "Le lien de l'avatar est trop long (Max 32000 caractères)",
+        "max_length": "Le lien de l'avatar est trop long",
         "min_length": "Le lien de l'avatar est trop court (Min 3 caractères)"
     }}
-    PASSWORD = {"label": "Mot de passe", "max_length": 256, "min_length": 3, "error_messages": {
+    PASSWORD = {"label": "Mot de passe", "max_length": 128, "min_length": 3, "error_messages": {
         "required": "Ce champ est obligatoire",
-        "max_length": "Le mot de passe est trop long (Max 256 caractères)",
+        "max_length": "Le mot de passe est trop long",
         "min_length": "Le mot de passe est trop court (Min 3 caractères)"
     }}
-    CONFIRM_PASSWORD = {"label": "Confirmation du mot de passe", "max_length": 256, "min_length": 3, "error_messages": {
+    CONFIRM_PASSWORD = {"label": "Confirmation du mot de passe", "max_length": 128, "min_length": 3, "error_messages": {
         "required": "Ce champ est obligatoire",
-        "max_length": "La confirmationd du mot de passe est trop long (Max 256 caractères)",
+        "max_length": "La confirmationd du mot de passe est trop long",
         "min_length": "La confirmationd du mot de passe est trop court (Min 3 caractères)"
     }}
