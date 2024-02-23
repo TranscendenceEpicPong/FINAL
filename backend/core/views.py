e8c7c6a5b793346c5d459916eb6eb7f91ba74492
user@pop-os <mon@email.com>
2024-02-23 03:08:16 +0100

    Save

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,7 +3,6 @@ from django.views.decorators.http import require_GET
 from django.middleware.csrf import get_token
 from django.views.decorators.http import require_http_methods
 from core.models import EpicPongUser as User
-from django.contrib.auth import update_session_auth_hash
 import json
 import jwt
 from backend.settings import env
@@ -16,6 +15,7 @@ from django.contrib.auth import \
     update_session_auth_hash
 import datetime
 from blocks.service import BlockService
+from .forms import UserUpdateForm
 
 @require_GET
 def server_info(request):
@@ -30,9 +30,6 @@ def server_info(request):
 @require_http_methods("GET")
 def search(request, username):
     owner = request.user
-
-    if owner is None:
-        return get_response({ "message": "Votre compte est invalide", "status": 403})
 
     user = User.objects.filter(username=username).first()
     block_service = BlockService(user)
@@ -62,42 +59,23 @@ def update(request):
         body = json.loads(request.body)
     except:
         return get_response(StatusError.BAD_JSON_FORMAT.value)
+    
+    form = UserUpdateForm(body, instance=current_user)
+    if not form.is_valid():
+        return get_response({"message": "Saisie incorrect", "status": 400})
+    form.update()
 
-    username = body.get("username")
-    avatar = body.get("avatar")
-    password = body.get("password")
-    confirm_password = body.get("confirm_password")
-
-    if not username and not avatar and not password and not confirm_password:
-        return get_response(StatusError.NO_DATA.value)
-
-    validation_username = UserService().validate_username(username, current_user.username)
-    if validation_username != StatusSuccess.USERNAME_SUCCESS_VALIDATION.value:
-        return get_response(validation_username)
-
-    validation_avatar = UserService().validate_avatar(avatar)
-    if validation_avatar != StatusSuccess.AVATAR_SUCCESS_VALIDATION.value:
-        return get_response(validation_avatar)
-
-    validation_password = UserService().validate_password(password)
-    if validation_password != StatusSuccess.PASSWORD_SUCCESS_VALIDATION.value:
-        return get_response(validation_password)
-
-    validation_confirm_password = UserService().validate_confirm_password(password, confirm_password)
-    if validation_confirm_password != StatusSuccess.PASSWORD_SUCCESS_VALIDATION.value:
-        return get_response(validation_confirm_password)
-
-    request.user.username = username
-    if avatar and len(avatar) > 0:
-        request.user.avatar = avatar
+    username = form.cleaned_data.get('username')
+    avatar = form.cleaned_data.get('avatar')
+    password = form.cleaned_data.get('password')
+    confirm_password = form.cleaned_data.get('confirm_password')
 
     if password and confirm_password and len(password) > 0 and password == confirm_password:
-        request.user.set_password(password)
         update_session_auth_hash(request, request.user)
-    request.user.save()
+
     response = JsonResponse({
         "id": request.user.id,
-        "username": request.user.username,
+        "username": username,
         "avatar": request.user.avatar,
     }, status=200)
 
